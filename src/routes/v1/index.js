cd2caf13f273deb4ba52dc4e731a71eac0398c0a
Lokendra Tiwari <lokendratiwari22844@gmail.com>
2024-06-21 09:13:56 +0530

    Protected comment creation

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,13 +3,13 @@ import { createTweet, getTweet } from "../../controllers/tweet-controller.js";
 import { toggleLike } from "../../controllers/like-controller.js";
 import { createComment } from "../../controllers/comment-controller.js";
 import { login, signup } from "../../controllers/auth-controller.js";
-import {authenticate} from "../../middlewares/authenticate.js"
+import { authenticate } from "../../middlewares/authenticate.js";
 const router = express.Router();
 
-router.post("/tweets" , authenticate , createTweet);
+router.post("/tweets", authenticate, createTweet);
 router.post("/likes/toggle", toggleLike);
-router.post('/comments',createComment);
-router.get('/tweets/:id',getTweet);
-router.post('/signup',signup);
-router.post('/login',login);
+router.post("/comments", authenticate, createComment);
+router.get("/tweets/:id", getTweet);
+router.post("/signup", signup);
+router.post("/login", login);
 export default router;
